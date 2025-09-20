3e1412db8f4aa4272a7063a7292c217f8981e763
Deepak Patil <Patildipak0791@gmail.com>
2025-09-20 18:45:32 +0000

    Initial code

---
+++ main.py
@@ -0,0 +1,16 @@
+#Create a simple Flask application
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    sample_data = {
+        'id': 1,
+        'name': 'Sample Item',
+        'description': 'This is a sample item.'
+    }
+    return jsonify(sample_data)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
